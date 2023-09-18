0d94c4a6ed83e223fb246686b12f650aa447fee8
Alexander Henting <a.henting@posteo.de>
2023-09-18 09:45:00 +0200

    final typescript commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./css/index.css";
+import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root") as HTMLElement
+  </React.StrictMode>
 );
